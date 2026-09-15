b6d29ec235fce2dd0cdb4437c9b51c8915c7b12d
hamidkhan41390 <hamidkhan41390@gmail.com>
2026-09-15 14:47:04 +0530

    Time: 1 ms (18.57%) | Memory: 43.5 MB (40.22%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,26 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-      bubble(nums);
+      insertion(nums);
         
     }
-      public void bubble(int []arr){
-        boolean swap;
-// run the steps n-1 times
-        for (int i = 0; i < arr.length; i++) {
-            swap=false;
-            // for each step, max item will come at the last respective index
-            for (int j = 1; j <= arr.length - i - 1; j++) {
-                //swap if the item is smaller than the previous item
+      public void swap(int []arr,int first ,int second ){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+     public void insertion(int []arr){
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j >0; j--) {
                 if (arr[j]<arr[j-1]){
-                    //swwap
-                    int temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
-                    swap=true;
+                    swap(arr,j,j-1);
+                }else {
+                    break;
                 }
-            }
-            if (!swap){
-                break;
             }
         }
 
